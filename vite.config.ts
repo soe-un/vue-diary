b2598ccf1e84f7f8e661d334cee1a07c6e3d2698
soe-un <runterf@gmail.com>
2025-08-07 19:51:09 +0900

    vite @ import error 해결 안 됨

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
